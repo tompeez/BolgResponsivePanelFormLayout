3fdd55d44602cd017b6e7c6f3a6d25a763eda05c
TimoHahn <timo.hahn@virtual7.de>
2020-03-09 10:38:55 +0100

    init

---
+++ BRPFLViewController/src/de/hahn/blog/responsivepanelformlayout/view/ResponsiveFormBean.java
@@ -0,0 +1,18 @@
+package de.hahn.blog.responsivepanelformlayout.view;
+
+
+public class ResponsiveFormBean {
+    String _layout = "WEIGHTED";
+
+    public ResponsiveFormBean() {
+        super();
+    }
+
+    public void setLayout(String _layout) {
+        this._layout = _layout;
+    }
+
+    public String getLayout() {
+        return _layout;
+    }
+}
